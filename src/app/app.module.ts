dcf050872e16c94f8ea1b1393b6227430e252476
f.bakal1997@gmail.com <f.bakal1997@gmail.com>
2022-06-07 23:48:05 +0300

    added primeflex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
+import { BrandComponent } from './components/brand/brand.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SidebarModule } from 'primeng/sidebar';
+
+import { MenuModule } from 'primeng/menu';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandComponent,
+    NavbarComponent,
+    SidebarComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule
+    ButtonModule,
+    SidebarModule,
+    MenuModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
